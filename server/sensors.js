6b56a3e735635ad4555c150538daab3965c27871
Serge Korzh <serge.korzh@gmail.com>
2017-05-26 15:37:28 +0200

    Upload test to Rpi

--- server/sensors.js
+++ server/sensors.js
@@ -4,7 +4,7 @@ const VIBRA = 11, LED = 12, LIGHT = 13
 
 rpio.open(LED, rpio.OUTPUT, rpio.LOW)
 rpio.open(VIBRA, rpio.INPUT)
-rpio.open(VIBRA, rpio.INPUT)
+rpio.open(LIGHT, rpio.INPUT)
 
 function main() {
   rpio.write(LED, rpio.read(VIBRA))
