a8e46a65e72cbab71d78186c8705914bdfa3a708
Serge Korzh <serge.korzh@gmail.com>
2017-05-27 12:57:16 +0200

    Add light test

--- server/characteristics/senact/lightIO.js
+++ server/characteristics/senact/lightIO.js
@@ -12,18 +12,22 @@ module.exports = {
   
   set: (value) => {
     this.isOn = value
-    // TODO: code that toggles lights
+    rpio.read(actuators.LED, value)
   },
   
   setModeAutomatic: (mode) => {
     this.isAutomatic = mode
+    conslole.log('Lights Mode: ' + (mode ? 'on' : 'off'))
     if (mode) {
       rpio.poll(sensors.LIGHT, (pin) => { this.set(rpio.read(pin)) }) // TOGGLE LIGHTS IF READ 1
     } else {
       rpio.poll(sensors.LIGHT, null)
     }
   }
-  
-  
-  
 }
+
+module.exports.init()
+module.exports.set(true)
+rpio.sleep(1)
+module.exports.set(false)
+module.exports.setAutomatic(true)
