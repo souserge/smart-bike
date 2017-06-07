78415d7aa5d6d33cd8d46e75db36affcb57c6d34
Serge Korzh <serge.korzh@gmail.com>
2017-06-07 14:29:22 +0200

    Update the Rpi

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const bleno = require('bleno')
 
-const TestCharacteristic = require('./characteristics/test')
-const LightCharacteristic = require('./characteristics/light')
-const AntiTheftCharacteristic = require('./characteristics/antiTheft')
+const testCharacteristic = require('./characteristics/test')
+const lightCharacteristic = require('./characteristics/light')
+const antiTheftCharacteristic = require('./characteristics/antiTheft')
 
 const ids = require('../global/ble').bleIds
 
-const testCharacteristic = new TestCharacteristic()
-const lightCharacteristic = new LightCharacteristic()
-const antiTheftCharacteristic = new AntiTheftCharacteristic()
+//const testCharacteristic = new TestCharacteristic()
+//const lightCharacteristic = new LightCharacteristic()
+//const antiTheftCharacteristic = new AntiTheftCharacteristic()
 
 const bikeService = new bleno.PrimaryService({
   uuid: ids.get('SERVICE').uuid,
